ec540ee693300d343eb43f251e29c66507f850d8
rolobio <rolobio@rolobio.com>
2016-11-17 16:37:00 -0700

    Adding setup.py script for pip uses.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from pgpydict import __version__, __doc__
+
+config = {
+    'name':'pgpydict',
+    'version':__version__,
+    'author':'rolobio',
+    'author_email':'rolobio+pgpydict@rolobio.com',
+    'description':'Access a Psycopg2 database as if it were a Python Dictionary',
+    'license':'Apache2',
+    'keywords':'psycopg2 dictionary',
+    'url':'https://github.com/rolobio/pgpydict',
+    'packages':[
+        'pgpydict',
+        ],
+    'long_description':__doc__,
+    'install_requires': [
+        'psycopg2',
+        ],
+    'classifiers':[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        ],
+    'test_suite':'pgpydict.test_pgpydict',
+    }
+
+setup(**config)
+
