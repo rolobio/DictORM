a63ffed9cd40a172004fee986d2a308d99ac11b7
rolobio <rolobio@rolobio.com>
2017-11-30 09:32:24 -0700

    Fixing test doc

--- dictorm/test/test_dictorm.py
+++ dictorm/test/test_dictorm.py
@@ -1259,7 +1259,7 @@ class TestPostgresql(CommonTests, unittest.TestCase):
 
     def test_varchar(self):
         """
-        A column name can't be used for injection
+        A varchar type raises and error when too many characters are passed.
         """
         Foo = self.db['foo']
         # bar is short enough
